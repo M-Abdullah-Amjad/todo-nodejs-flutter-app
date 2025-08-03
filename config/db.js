9ec9fb4a2d26aa2261ee011eece1c3638cd2ad72
zeeshan41571 <abdullahamjadlhr2020@gmail.com>
2025-08-03 16:30:36 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 const express = require('express');
 
 // || 'mongodb://127.0.0.1:27017/newTodoList'
-const connection = mongoose.createConnection(process.env.MONGODB_URI ).on('open',()=>{
+const connection = mongoose.createConnection(process.env.MONGODB_URI || 'mongodb+srv://abdullahamjadlhr2020:J7oH8Wn1zmPoC4VD@todolistcluster.5zatdeo.mongodb.net/?retryWrites=true&w=majority&appName=todolistcluster').on('open',()=>{
     console.log('Connected to MongoDB');
 }).on('error', (err) => {
     console.error('MongoDB connection error:', err);

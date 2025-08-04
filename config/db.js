f2b5bada8431a1b7d6f0a8f29801791d6740d3b9
Muhammad Abdullah Amjad <abdullahamjadlhr2020@gmail.com>
2025-08-04 20:01:50 +0500

    Update 4/8/25

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 
-// || 'mongodb://127.0.0.1:27017/newTodoList'
-const connection = mongoose.createConnection(process.env.MONGODB_URI || 'mongodb+srv://abdullahamjadlhr2020:J7oH8Wn1zmPoC4VD@todolistcluster.5zatdeo.mongodb.net/?retryWrites=true&w=majority&appName=todolistcluster').on('open',()=>{
+
+const connection = mongoose.createConnection(process.env.MONGODB_URI).on('open',()=>{
     console.log('Connected to MongoDB');
 }).on('error', (err) => {
     console.error('MongoDB connection error:', err);
